b7e8c36b28a1ef2f73f98f9f2b4d5fc750b9be33
jjdavenport <jordanjamesdavenport@gmail.com>
2023-12-30 14:08:47 +0000

    09 complete.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function(a) {
+const format = a.toLowerCase().replace(/[\W_]/g, '')
+const reverse = format.split('').reverse().join('')
+return format === reverse
+}
 // Do not edit below this line
 module.exports = palindromes;
