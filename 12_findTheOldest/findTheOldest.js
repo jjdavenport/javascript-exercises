9e0cfe1bbaee557d848d5b7353d7e69c8ceb07f2
jjdavenport <jordanjamesdavenport@gmail.com>
2023-12-31 16:30:29 +0000

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,6 @@
-const findTheOldest = function(arr) {
+const findTheOldest = function(people) {
+    const oldest = people.sort((a,b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
+    return oldest[0];
 }
 
 // Do not edit below this line
